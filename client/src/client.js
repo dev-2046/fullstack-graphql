8b3d2e55a12ddc3c29e0a75991dd3977c4931f40
Scott Moss <willscottmoss@gmail.com>
2019-10-13 20:08:19 -0500

    ready

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,8 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
-/**
- * Create a new apollo client and export as default
- */
+const client = new ApolloClient();
+
+export default client;
